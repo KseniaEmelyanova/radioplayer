0639883877770fd7839ab12db6af7bd6c889ea8b
Alexander Schilinski <contact@schilinskialexander.com>
2023-04-20 18:13:59 +0200

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,1 +1,75 @@
+<?php
 
+include "settings.php"; 
+
+$Json = file_get_contents("https://".$lclink.":".$lcport."/".$lcapilink."/servers/?format=json");
+$myarray = json_decode($Json, true);
+
+foreach($myarray AS $value) {
+    if ($value["id"] == "1") {
+        $title1 = $value["title"]; 
+
+    } elseif ($value["id"] == "2") {
+        $title2 = $value["title"]; 
+
+    } 
+
+}
+
+$Json = file_get_contents("https://".$lclink.":".$lcport."/".$lcapilink."/channels/?format=json");
+$myarray = json_decode($Json, true);
+
+foreach($myarray AS $value) {
+    if ($value["id"] == $myarray[0]["id"] && $value["server"] !== 2) {
+        $channel1 = $value["bitrate"]; 
+        $asxlink1 = $value["links_html"]["WindowsMedia"]["link"]; 
+        $m3ulink1 = $value["links_html"]["Winamp"]["link"]; 
+        $ramlink1 = $value["links_html"]["RealPlayer"]["link"]; 
+        $weblink1 = $value["links_html"]["WebPlayer"]["link"];
+           
+    } 
+    if ($value["id"] == $myarray[1]["id"] && $value["server"] !== 2) {
+        $channel2 = $value["bitrate"];
+        $asxlink2 = $value["links_html"]["WindowsMedia"]["link"]; 
+        $m3ulink2 = $value["links_html"]["Winamp"]["link"]; 
+        $ramlink2 = $value["links_html"]["RealPlayer"]["link"]; 
+        $weblink2 = $value["links_html"]["WebPlayer"]["link"]; 
+          
+    }
+    if ($value["server"] !== 1) {
+        $channel3 = $value["bitrate"];
+        $asxlink3 = $value["links_html"]["WindowsMedia"]["link"]; 
+        $m3ulink3 = $value["links_html"]["Winamp"]["link"]; 
+        $ramlink3 = $value["links_html"]["RealPlayer"]["link"]; 
+        $weblink3 = $value["links_html"]["WebPlayer"]["link"];   
+            
+    } 
+    
+}
+
+$Json = file_get_contents("https://".$lclink.":".$lcport."/".$lcapilink."/history/?format=json");
+$myarray = json_decode($Json, true);
+
+foreach($myarray AS $value) {
+    if ($value["id"] == $myarray[0]["id"] && $value["server"] !== 2) {
+        $titleonair = $value["metadata"]; 
+        $titlepic = $value["img_url"]; 
+        $votid = $value["all_music_id"];
+           
+    } 
+     
+}
+
+$Json = file_get_contents("https://".$lclink.":".$lcport."/".$lcapilink."/djs/?format=json");
+$myarray = json_decode($Json, true);
+
+foreach($myarray AS $value) {
+    if ($value["id"] == $myarray[0]["id"] && $value["server"] !== 2) {
+        $onairdj = $value["name"]; 
+        $djpic = $value["image"];
+        
+    } 
+      
+}
+
+?>
