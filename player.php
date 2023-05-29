3f76cd3b2598363043f2492b41e353c364742563
Alexander Schilinski <contact@schilinskialexander.com>
2023-05-29 11:45:11 +0200

    Update player.php

--- player.php
+++ player.php
@@ -21,19 +21,19 @@ include __DIR__.'/api/api.php';
                 <h2><span>OnAir: <?= $onairdj; ?></span><br><?= $titleonair; ?></h2>
                 <div class="data">
                     <a href="<?= $asxlink1; ?>">
-                    <img src="<?= lclp; ?>/media/static/images/listener/mplayer.png" title="WindowsMedia 128 kpbs mp3"></a>
+                    <img src="<?= $lclp; ?>/media/static/images/listener/mplayer.png" title="WindowsMedia 128 kpbs mp3"></a>
 
                     <a href="<?= $m3ulink1; ?>">
-                    <img src=<?= lclp; ?>/media/static/images/listener/winamp.png" title="Winamp 128 kpbs mp3"></a>
+                    <img src="<?= $lclp; ?>/media/static/images/listener/winamp.png" title="Winamp 128 kpbs mp3"></a>
 
                     <a href="<?= $m3ulink1; ?>">
-                    <img src="<?= lclp; ?>/media/static/images/listener/itunes.png" title="iTunes 128 kpbs mp3"></a>
+                    <img src="<?= $lclp; ?>/media/static/images/listener/itunes.png" title="iTunes 128 kpbs mp3"></a>
                     
                     <a href="<?= $asxlink1; ?>">
-                    <img src="<?= lclp; ?>/media/static/images/listener/realplayer.png"; title="RealPlayer 128 kpbs mp3"></a>
+                    <img src="<?= $lclp; ?>/media/static/images/listener/realplayer.png"; title="RealPlayer 128 kpbs mp3"></a>
 
                     <a href="<?= $weblink1; ?>" target="_blank">
-                    <img src="<?= lclp; ?>/media/static/images/listener/web.png" title="WebPlayer 128 kpbs mp3"></a>
+                    <img src="<?= $lclp; ?>/media/static/images/listener/web.png" title="WebPlayer 128 kpbs mp3"></a>
                     
                 </div>
                 <div class="actionBtn">
