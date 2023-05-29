48314a049423914263e0f2afb529de6c0e327299
Alexander Schilinski <contact@schilinskialexander.com>
2023-05-29 11:29:35 +0200

    Update settings.php

--- api/settings.php
+++ api/settings.php
@@ -2,6 +2,9 @@
 
 $lclink = "demoaccount.s02.radio-tochka.com"; //Link
 $lcport = "8080"; //Port
-$lcapilink = "api/v2"; //api version
+$lcapilink = "/api/v2"; //api version
+
+$lcapicom = 'https://' . $lclink . ':' . $lcport . $lcapilink ;
+$lclp = 'https://' . $lclink . ':' . $lcport ;
 
 ?>
