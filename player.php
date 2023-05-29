df99bf87ebd82be19386fd8ed6eeba8d43d8ff86
Alexander Schilinski <contact@schilinskialexander.com>
2023-05-29 11:37:39 +0200

    Update player.php

--- player.php
+++ player.php
@@ -11,29 +11,29 @@ include __DIR__.'/api/api.php';
     <div class="card">
         <div class="lines"></div>
         <div class="imgBx">
-            <img src="<?php echo $djpic; ?>">
+            <img src="<?= $djpic; ?>">
         </div>
         <div class="imgBx1">
-            <img src="<?php echo $titlepic; ?>">
+            <img src="<?= $titlepic; ?>">
         </div>
         <div class="content">
             <div class="details">
-                <h2><span>OnAir: <?php echo $onairdj; ?></span><br><?php echo $titleonair; ?></h2>
+                <h2><span>OnAir: <?= $onairdj; ?></span><br><?= $titleonair; ?></h2>
                 <div class="data">
-                    <a href="<?php echo $asxlink1; ?>">
-                    <img src=<?php echo "https://".$lclink.":".$lcport."/media/static/images/listener/mplayer.png" ?> title="WindowsMedia 128 kpbs mp3"></a>
+                    <a href="<?= $asxlink1; ?>">
+                    <img src="<?= lclp; ?>/media/static/images/listener/mplayer.png" title="WindowsMedia 128 kpbs mp3"></a>
 
-                    <a href="<?php echo $m3ulink1; ?>">
-                    <img src=<?php echo "https://".$lclink.":".$lcport."/media/static/images/listener/winamp.png" ?> title="Winamp 128 kpbs mp3"></a>
+                    <a href="<?= $m3ulink1; ?>">
+                    <img src=<?= lclp; ?>/media/static/images/listener/winamp.png" title="Winamp 128 kpbs mp3"></a>
 
-                    <a href="<?php echo $m3ulink1; ?>">
-                    <img src=<?php echo "https://".$lclink.":".$lcport."/media/static/images/listener/itunes.png" ?> title="iTunes 128 kpbs mp3"></a>
+                    <a href="<?= $m3ulink1; ?>">
+                    <img src="<?= lclp; ?>/media/static/images/listener/itunes.png" title="iTunes 128 kpbs mp3"></a>
                     
-                    <a href="<?php echo $asxlink1; ?>">
-                    <img src=<?php echo "https://".$lclink.":".$lcport."/media/static/images/listener/realplayer.png"; ?> title="RealPlayer 128 kpbs mp3"></a>
+                    <a href="<?= $asxlink1; ?>">
+                    <img src="<?= lclp; ?>/media/static/images/listener/realplayer.png"; title="RealPlayer 128 kpbs mp3"></a>
 
-                    <a href="<?php echo $weblink1; ?>" target="_blank">
-                    <img src=<?php echo "https://".$lclink.":".$lcport."/media/static/images/listener/web.png" ?> title="WebPlayer 128 kpbs mp3"></a>
+                    <a href="<?= $weblink1; ?>" target="_blank">
+                    <img src="<?= lclp; ?>/media/static/images/listener/web.png" title="WebPlayer 128 kpbs mp3"></a>
                     
                 </div>
                 <div class="actionBtn">
